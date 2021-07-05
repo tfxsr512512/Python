ac8653bee447ecdbe7e67b0e1f676122308a8388
tfxsr512512 <45411374+tfxsr512512@users.noreply.github.com>
2021-07-05 19:27:50 +0800

    Add files via upload

---
+++ Python/code/day1/bool.py
@@ -0,0 +1,7 @@
+score = 560
+qing_hua_score_benchmark = 680
+if score >= qing_hua_score_benchmark: # True
+	print("congratulations.你上了北大......")
+
+else: # False
+	print("你只能上北大青鸟......")
---
+++ Python/code/day1/hello.py
@@ -0,0 +1,7 @@
+print('hello world.')
+
+for i in range(10):
+	print("run",i)
+
+# import time
+# time.sleep(10)
